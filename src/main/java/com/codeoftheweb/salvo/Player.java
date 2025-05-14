1a472b111b281b68379df03a16b04857a933b2de
Dani Barisic <barisicdani@gmail.com>
2025-05-14 14:31:16 +0200

    Added ID generation

--- src/main/java/com/codeoftheweb/salvo/Player.java
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -1,10 +1,15 @@
 package com.codeoftheweb.salvo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Player {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
   private String userName;
 
   public String getUserName() {
