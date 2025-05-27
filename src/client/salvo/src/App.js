5a0459f8af83ad7edf383e38a895f650903dded0
Dani Barisic <barisicdani@gmail.com>
2025-05-27 10:59:36 +0200

    Added React Build Files

---
+++ src/client/salvo/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { GameList } from './components/gamesList.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <GameList />
+    </div>
+  );
+}
+
+export default App;
