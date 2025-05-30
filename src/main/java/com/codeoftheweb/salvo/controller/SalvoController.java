bcf508b81c9a03fa45c830f6b6fc3b9f3d8aab4b
Dani Barisic <barisicdani@gmail.com>
2025-05-30 17:06:54 +0100

    Corrected from gamePlayer id to actual game id

--- src/main/java/com/codeoftheweb/salvo/controller/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/SalvoController.java
@@ -2,11 +2,16 @@ package com.codeoftheweb.salvo.controller;
 
 import com.codeoftheweb.salvo.GameRepository;
 import com.codeoftheweb.salvo.Player;
+import com.codeoftheweb.salvo.Game;
+import com.codeoftheweb.salvo.GamePlayerRepository;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 import java.util.stream.Collectors;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -20,6 +25,47 @@ public class SalvoController {
     @Autowired
     private GameRepository gameRepository;
 
+    @GetMapping("/game_view/{gameId}")
+    public Map<String, Object> getGameView(@PathVariable Long gameId) {
+
+        Game game = gameRepository.findById(gameId).orElseThrow();
+
+        Map<String, Object> dto = new LinkedHashMap<>();
+        dto.put("gameId", game.getId());
+        dto.put("created", game.getCreatedDate());
+
+        // Map gamePlayers.
+        List<Map<String, Object>> gamePlayers = game.getGamePlayers().stream()
+                .map(gp -> {
+                    Map<String, Object> gamePlayerDto = new LinkedHashMap<>();
+                    gamePlayerDto.put("id", gp.getId());
+
+                    Player player = gp.getPlayer();
+                    Map<String, Object> playerDto = new LinkedHashMap<>();
+                    playerDto.put("id", player.getId());
+                    playerDto.put("email", player.getEmail());
+
+                    gamePlayerDto.put("player", playerDto);
+                    return gamePlayerDto;
+                })
+                .collect(Collectors.toList());
+
+        dto.put("gamePlayers", gamePlayers);
+
+        List<Map<String, Object>> ships = game.getGamePlayers().stream()
+                .flatMap(gp -> gp.getShips().stream())
+                .map(ship -> {
+                    Map<String, Object> shipDto = new LinkedHashMap<>();
+                    shipDto.put("type", ship.getType());
+                    shipDto.put("locations", ship.getLocation());
+                    return shipDto;
+                })
+                .collect(Collectors.toList());
+
+        dto.put("ships", ships);
+        return dto;
+    }
+
     @GetMapping("/games")
     public List<Map<String, Object>> getGames() {
         return gameRepository
@@ -27,9 +73,8 @@ public class SalvoController {
                 .stream()
                 .map(game -> {
                     Map<String, Object> dto = new LinkedHashMap<>();
-                    dto.put("id", game.getId());
-                    dto.put("ships", game.getShips());
-                    dto.put("created", game.getCreatedDate());
+                    dto.put("id", game.getId()); // add id key
+                    dto.put("created", game.getCreatedDate()); // add created key
 
                     List<Map<String, Object>> gamePlayers = game.getGamePlayers()
                             .stream()
