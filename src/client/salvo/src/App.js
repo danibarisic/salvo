08e55bbca8b4b7f7ceb6e6c9591afb99cb4f770f
Dani Barisic <barisicdani@gmail.com>
2025-06-04 19:45:07 +0200

    Passing opponentSalvoes into CreateGrid

--- src/client/salvo/src/App.js
+++ src/client/salvo/src/App.js
@@ -4,17 +4,29 @@ import './App.css';
 import { GameInfo, CreateGrid } from './components/game.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { CreateGridSalvo } from './components/createGridSalvo.jsx';
+
 function App() {
   const [playerShips, setPlayerShips] = useState([]);
+  const [salvoLocations, setSalvoLocations] = useState({ player: [], opponent: [] });
+  //initializes empty arrays for player and opponent.
+
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<h1>Please enter 'localhost3000/game_view/PlayerId' into the URL</h1>} />
           <Route path="/game_view/:gpId"
             element={
               <>
-                <GameInfo setPlayerShips={setPlayerShips} />
-                <CreateGrid playerShips={playerShips} />
+                <GameInfo
+                  setPlayerShips={setPlayerShips}
+                  setSalvoLocations={setSalvoLocations} />
+                <CreateGrid
+                  playerShips={playerShips}
+                  opponentSalvoes={salvoLocations.opponent}
+                />
+                <CreateGridSalvo playerSalvoes={salvoLocations?.player || []} />
               </>
             }
           />
