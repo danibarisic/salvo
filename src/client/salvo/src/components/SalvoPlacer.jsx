b9eaba849ebf3a0886a79f23b0d1dc524e44d877
Dani Barisic <barisicdani@gmail.com>
2025-07-02 20:31:51 +0200

    Removed useEffect

--- src/client/salvo/src/components/SalvoPlacer.jsx
+++ src/client/salvo/src/components/SalvoPlacer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export const SalvoPlacer = ({ gamePlayerId, playerSalvoes, previouslyFired, salvoSubmitted }) => {
     const [selectedCells, setSelectedCells] = useState([]);
